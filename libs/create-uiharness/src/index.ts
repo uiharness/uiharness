8f3f056f41399a656d41e90c6b071527c01c520b
Phil Cockfield <phil@cockfield.net>
2019-01-14 21:42:25 +1300

    Run yarn install on bootstrapper.

--- libs/create-uiharness/src/index.ts
+++ libs/create-uiharness/src/index.ts
@@ -5,11 +5,29 @@ export async function create() {
   log.info();
   const targetDir = fsPath.resolve('.');
   const settingsPath = fsPath.join(__dirname, '../templates.yml');
-  await newFile.create({
+  const res = await newFile.create({
     targetDir,
     settingsPath,
     beforeWrite,
   });
+  log.info();
+
+  if (!res.success) {
+    log.error.yellow(`😥  Failed to create UIHarness from template.`);
+    if (res.error) {
+      log.error(res.error.message);
+    }
+  }
+
+  log.info('res', res);
+  log.info('-------------------------------------------');
+  log.info('res.dir', res.dir);
+  log.info();
+
+  log.info.gray('Now run:\n');
+  log.info.cyan(`   cd ${log.white(fsPath.basename(res.dir))}`);
+  log.info.cyan(`   yarn start`);
+  log.info();
 }
 
 /**
