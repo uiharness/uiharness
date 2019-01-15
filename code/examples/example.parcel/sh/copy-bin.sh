fafe58bbb23269aae12cf3fc4adf8f77b1e21c3b
Phil Cockfield <phil@cockfield.net>
2019-01-15 13:44:39 +1300

    Eamples

--- code/examples/example.parcel/sh/copy-bin.sh
+++ code/examples/example.parcel/sh/copy-bin.sh
@@ -4,5 +4,5 @@
 # DEBUG: Copy updates to the .bin/uiharness files during development
 # 
 
-cp ../../libs/parcel/lib/bin/uiharness.js ./node_modules/.bin/uiharness
+cp ../../libs/parcel/lib/bin.js ./node_modules/.bin/uiharness
 chmod 777 ./node_modules/.bin/uiharness
