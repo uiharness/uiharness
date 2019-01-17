5d64b00dd1905528b603f6bfac1ba2068597ff9d
Phil Cockfield <phil@cockfield.net>
2019-01-17 19:02:48 +1300

    Engines

--- code/libs/electron/tmpl/src/renderer/index.tsx
+++ code/libs/electron/tmpl/src/renderer/index.tsx
@@ -1,9 +1,6 @@
 import './css/normalize.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
-// import * as React from 'react';
-
 import { HelloWorld } from './screens/HelloWorld';
 
 const el = <HelloWorld />;
