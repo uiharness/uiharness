e10389f4aff8fbb96ae98f2685df09d59d1cb9f3
Phil Cockfield <phil@cockfield.net>
2019-01-21 20:50:39 +1300

    run builder

--- code/libs/electron/src/bin/cmds/dist.ts
+++ code/libs/electron/src/bin/cmds/dist.ts
@@ -1,8 +1,33 @@
-import { Settings } from '../common';
+import {
+  parcel,
+  log,
+  Settings,
+  Package,
+  logInfo,
+  fsPath,
+  constants,
+  electron,
+} from '../common';
+import { init } from './init';
 
 /**
  * Bundles the application ready for distribution.
  */
-export async function dist(args: { settings: Settings }) {
-  console.log('\n\n DIST !!\n\n');
+export async function dist(args: { settings: Settings; pkg: Package }) {
+  const { settings, pkg } = args;
+  const mainEntry = fsPath.resolve(constants.PATH.MAIN_ENTRY);
+  process.env.NODE_ENV = 'production';
+
+  // Ensure the module is initialized.
+  await init({ settings, pkg });
+  logInfo({ settings, pkg, port: false, mainEntry });
+
+  // Build JS bundles and run the electorn-builder.
+  await parcel.build(settings, { isProd: true });
+  log.info();
+  await electron.build();
+
+  // Finish up.
+  log.info();
+  process.exit(0);
 }
