cebcdd6941f9f3493bf5a8b20d5bd399f6e68124
Phil Cockfield <phil@cockfield.net>
2019-01-23 11:44:36 +1300

    building the dist

--- code/libs/electron/src/bin/cmds/clean.ts
+++ code/libs/electron/src/bin/cmds/clean.ts
@@ -7,8 +7,6 @@ export async function clean(args: {}) {
   await fs.remove(fsPath.resolve('./.uiharness'));
 
   // TEMP 🐷
-  await fs.remove(fsPath.resolve('./.cache'));
-  await fs.remove(fsPath.resolve('./dist'));
   await fs.remove(fsPath.resolve('./src/main/.parcel'));
   await fs.remove(fsPath.resolve('./src/renderer/.parcel'));
 }
