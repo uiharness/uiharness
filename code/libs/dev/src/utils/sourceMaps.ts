af44421df7ea57758961c4c850f1f1be2d8b825f
Phil Cockfield <phil@cockfield.net>
2019-02-06 17:45:39 +1300

    removeSourceMapRefs

---
+++ code/libs/dev/src/utils/sourceMaps.ts
@@ -0,0 +1,72 @@
+import { fs } from '@platform/fs';
+
+export type IRemoveSourceMapsResponse = {
+  changed: Array<{ path: string }>;
+};
+
+/**
+ * Strips source maps references from JS files within the given path.
+ * NOTE:
+ *    This is useful when referenced modules has published JS files
+ *    with references to source-maps but not actually published
+ *    the `.map.js` files.
+ *
+ *    Removing the references from the files will prevent annoying warnings
+ *    in the console when Parcel bundles.
+ *
+ * Example:
+ *
+ *    await utils.removeSourceMapRefs('node_modules/rxjs');
+ *
+ */
+export async function removeSourceMapRefs(dir: string) {
+  const result: IRemoveSourceMapsResponse = { changed: [] };
+  const paths = await fs.glob.find(fs.join(dir, '**/*.js'));
+  for (const path of paths) {
+    const wasRemoved = await removeSourceMapRef(path);
+    if (wasRemoved) {
+      result.changed = [...result.changed, { path }];
+    }
+  }
+  return result;
+}
+
+/**
+ * Removes the source-map ref from theh given file.
+ */
+export async function removeSourceMapRef(file: string) {
+  try {
+    const text = await fs.readFile(file, 'utf8');
+    const lines = text.split('\n');
+    let isChanged = false;
+
+    lines.forEach((line, i) => {
+      if (isSourceMapRef(line)) {
+        lines[i] = '\n';
+        isChanged = true;
+      }
+    });
+
+    if (isChanged) {
+      await fs.writeFile(file, lines.join('\n'));
+    }
+
+    return isChanged;
+  } catch (error) {
+    // Ignore.
+    return false;
+  }
+}
+
+/**
+ * INTERNAL
+ */
+function isSourceMapRef(line: string) {
+  return line.includes('//# sourceMappingURL=');
+}
+
+/**
+ * TEMP
+ */
+removeSourceMapRefs('node_modules/monaco-editor/esm');
+removeSourceMapRefs('node_modules/rxjs');
