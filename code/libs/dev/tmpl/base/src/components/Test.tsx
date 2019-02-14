ffa51ce6b044213988317f17e8fdab5a2efbf47c
Phil Cockfield <phil@cockfield.net>
2019-02-14 23:35:33 +1300

    sample starting point.

--- code/libs/dev/tmpl/base/src/components/Test.tsx
+++ code/libs/dev/tmpl/base/src/components/Test.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Button, ObjectView } from '@uiharness/electron/lib/renderer';
 
 /**
  * Sample starting point.
@@ -9,10 +10,26 @@ import * as React from 'react';
  *    - [.uiharness.yml]
  *
  */
-export const Test = () => {
-  return (
-    <div style={{ paddingLeft: 20 }}>
-      <h1>Hello World!</h1>
-    </div>
-  );
-};
+
+export type IState = { count?: number };
+export class Test extends React.PureComponent<{}, IState> {
+  public state: IState = { count: 0 };
+
+  public render() {
+    return (
+      <div style={{ paddingLeft: 25, lineHeight: 1.5 }}>
+        <h1>Hello World!</h1>
+        <Button label={'Increment'} onClick={this.increment(1)} />
+        <Button label={'Decrement'} onClick={this.increment(-1)} />
+        <ObjectView name={'state'} data={this.state} />
+      </div>
+    );
+  }
+
+  private increment = (by: number) => {
+    return () => {
+      const count = (this.state.count || 0) + by;
+      this.setState({ count });
+    };
+  };
+}
