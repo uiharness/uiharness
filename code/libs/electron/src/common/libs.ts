3b9c98fd98ba713c7d35d7e89b163c16032796a2
Phil Cockfield <phil@cockfield.net>
2019-02-20 09:02:27 +1300

    refs

--- code/libs/electron/src/common/libs.ts
+++ code/libs/electron/src/common/libs.ts
@@ -3,4 +3,4 @@ import * as path from 'app-root-path';
 export { path };
 export { is } from '@platform/util.is';
 export { time, value } from '@platform/util.value';
-export { css, GlamorValue, color } from '@platform/util.react';
+export { css, GlamorValue, color } from '@platform/react';
