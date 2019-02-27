0a95aebc5bd1300cbaefc604458b11d116562b58
Phil Cockfield <phil@cockfield.net>
2019-02-28 11:33:29 +1300

    remove line

--- code/libs/dev/templates/base/test/renderer.tsx
+++ code/libs/dev/templates/base/test/renderer.tsx
@@ -1,6 +1,5 @@
 import '@babel/polyfill';
 import '../node_modules/@uiharness/electron/css/normalize.css';
-
 import * as React from 'react';
 import { renderer } from '@platform/electron/lib/renderer';
 import { Test } from '../src/components/Test';
