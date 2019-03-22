da12fee19709ec8684d5d9008d1cc15c3bae89ab
Phil Cockfield <phil@cockfield.net>
2019-03-22 15:18:50 +1300

    update call-site

--- code/create/create-ui/src/common/libs.ts
+++ code/create/create-ui/src/common/libs.ts
@@ -1,5 +1,4 @@
 export { log } from '@platform/log/lib/server';
 export { time } from '@platform/util.value';
-export { exec } from '@platform/util.exec';
-export { npm, NpmPackage, INpmPackageFields } from '@platform/npm';
+export { npm, NpmPackage, INpmPackageFields, exec } from '@platform/npm';
 export { fs, Listr, prompt, Template, TemplateMiddleware } from 'create-tmpl';
