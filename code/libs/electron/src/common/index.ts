6f831eb12c4587995bb690e9c85955a541609c96
Phil Cockfield <phil@cockfield.net>
2019-04-16 22:40:25 +1200

    templateString helper

--- code/libs/electron/src/common/index.ts
+++ code/libs/electron/src/common/index.ts
@@ -1,6 +1,7 @@
 import * as constants from './constants';
+import * as templateString from './templateString';
 
-export { constants };
+export { constants, templateString };
 export * from './libs';
 export * from '../types';
 
