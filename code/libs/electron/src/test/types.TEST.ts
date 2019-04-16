6aa0ac02dcb1019c2d01fe4537ba1d8720e651bc
Phil Cockfield <phil@cockfield.net>
2019-04-16 22:39:24 +1200

    fix test

--- code/libs/electron/src/test/types.TEST.ts
+++ code/libs/electron/src/test/types.TEST.ts
@@ -5,6 +5,7 @@ describe('types', () => {
   it('main', () => {
     const config: uiharness.IRuntimeConfig = {
       name: 'Foo',
+      version: '1.2.3',
       electron: {
         port: 123,
         main: 'main.ts',
@@ -12,6 +13,7 @@ describe('types', () => {
       },
     };
     expect(config.name).to.eql('Foo');
+    expect(config.version).to.eql('1.2.3');
     expect(config.electron.renderer.default.title).to.eql('Foo');
     expect(config.electron.renderer.default.path).to.eql('renderer.tsx');
   });
