2e76a2b1a08120166b0e7621e3cd45dc8fed57c4
Phil Cockfield <phil@cockfield.net>
2019-06-13 18:20:44 +1200

    Upgrade tempaltes

--- code/libs/dev/templates/base/test/common/libs.ts
+++ code/libs/dev/templates/base/test/common/libs.ts
@@ -1,3 +1,6 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 export { Button, ObjectView, CommandShell, CommandState, Command, Hr } from '@uiharness/ui';
 export { log } from '@platform/log/lib/client';
 export * from '../../src/common';
