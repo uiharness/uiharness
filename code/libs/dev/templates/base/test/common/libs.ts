7385d65e777bdb13f3f828fc445d776803ca6dbc
Phil Cockfield <phil@cockfield.net>
2019-06-14 22:30:25 +1200

    bump

--- code/libs/dev/templates/base/test/common/libs.ts
+++ code/libs/dev/templates/base/test/common/libs.ts
@@ -1,5 +1,4 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+import '@platform/libs/polyfill';
 
 export { Button, ObjectView, CommandShell, CommandState, Command, Hr } from '@uiharness/ui';
 export { log } from '@platform/log/lib/client';
