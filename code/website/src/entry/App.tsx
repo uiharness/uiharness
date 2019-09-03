7c1c4380945eb3d9ff97b1e8448d194363b4e8fd
Phil Cockfield <phil@cockfield.net>
2019-09-03 19:28:32 +1200

    initial state.

--- code/website/src/entry/App.tsx
+++ code/website/src/entry/App.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
 
-import { shell } from './common';
+import { shell, COLORS } from './common';
 import * as splash from './splash';
 
 shell
   // Register application modules.
+  .main('main', () => import('./modules/main'))
   .register('Doc', () => import('./modules/Doc'))
   .register('Home', () => import('./modules/Home'))
   .register('Sidebar', () => import('./modules/Sidebar'))
-  .register('main', () => import('./modules/main'))
-  .default('Home');
+  .initial({
+    tree: { width: 0 },
+    body: { background: COLORS.DARK },
+    sidebar: { width: 0 },
+  });
 
 export const App = () => {
   return <shell.Loader splash={splash.factory} theme={'DARK'} loadDelay={1000} />;
