65f89a48cb97ed3b961515d07e04c9a724ab627c
Phil Cockfield <phil@cockfield.net>
2020-01-30 10:32:26 +1300

    clean up placeholders

--- code/create/.placeholders/create-harness/src/bin.ts
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-import { init } from '.';
-init();
--- code/create/.placeholders/create-uiharness/src/common/libs.ts
+++
@@ -1,5 +0,0 @@
-import * as fsPath from 'path';
-import * as inquirer from 'inquirer';
-
-export { fsPath, inquirer };
-export { log } from '@tdb/log/lib/server';
